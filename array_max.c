1e9384e78c0ef1ee989c35427ef5ab2894f0730f
m00849351-hub <m00849351@gmail.com>
2026-02-01 12:09:49 +0530

    array_max.c

---
+++ array_max.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int arr[100], n, i, max;
+    
+    printf("Enter number of element: ");
+    scanf("%d", &n);
+    
+    printf("Enter %d element:\n", n);
+    
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    max = arr[0];
+    for(i = 1; i < n; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    printf("Largest element = %d\n", max);
+    return 0;
+}
